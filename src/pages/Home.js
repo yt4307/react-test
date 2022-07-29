cb49790863b11eacd14c0cf4eec45ce3b4c8d550
정연한 <yt4307@gmail.com>
2022-07-29 19:56:22 +0900

    7/29 수업 자료

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+    text-align: center;
+`;
+
+const Home = () => {
+    return (
+        <Main>
+            홈 입니다.
+        </Main>
+    );
+};
+
+export default Home;
